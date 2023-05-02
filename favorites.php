4a657664c941ce953334bb993b9dabd5bb0104b0
VerenaMalak <127668837+VerenaMalak@users.noreply.github.com>
2023-05-02 20:19:53 +0200

    favorites

---
+++ favorites.php
@@ -0,0 +1,54 @@
+<?php require_once("php-functions/connection.php"); ?>
+
+
+<!DOCTYPE html>
+<html>
+
+<?php include("php-includes/head-tags.php"); ?>
+
+<body>
+<?php include("php-includes/navbar.php"); ?>
+
+<!-- Hero Start -->
+<section class="bg-half-170 d-table w-100" style="background: url('Images/favorites.jpg') center ; ">
+            <div class="bg-overlay bg-gradient-overlay"></div>
+            <div class="container">
+                <div class="row mt-5 justify-content-center">
+                    <div class="col-12">
+                        <div class="title-heading text-center">
+                            <h5 class="heading fw-semibold mb-0 page-heading text-white title-dark">Your Favourites</h5>
+                        </div>
+                    </div><!--end col-->
+                </div><!--end row-->
+            </div><!--end container-->
+        </section><!--end section-->
+        <!-- Hero End -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("php-includes/scripts.php"); ?>
+
+
+</body>
+</html>
