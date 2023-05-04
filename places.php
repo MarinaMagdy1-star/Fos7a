9c39aa43bfd4b383a550436c51062de82dcef802
VerenaMalak <127668837+VerenaMalak@users.noreply.github.com>
2023-05-04 17:25:49 +0200

    tzbet el fav

--- places.php
+++ places.php
@@ -72,6 +72,15 @@
                         <div class="card blog blog-primary shadow rounded overflow-hidden">
                             <div class="image position-relative overflow-hidden">
                                 <img src="Images/<?php echo $place['images']?>" class="img-fluid" alt="">
+                                
+
+                                <div class="blog-tag">
+                                    <a href="javascript:void(0)" class="badge bg-dark"><?php echo $place['name'] ?></a>
+                                </div>
+                            </div>
+                            
+
+                            <div class="card-body content">
                                 <!--button el favorite-->
                                
                                 <button type="button" onclick="addToFav(<?php echo $place['id']?>)" class="favorite-btn">
@@ -79,19 +88,11 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-heart-fill" viewBox="0 0 16 16">
                                     <path d="M2 15.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v13.5zM8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
                                     </svg>
-                                    </button>
-
-                                <div class="blog-tag">
-                                    <a href="javascript:void(0)" class="badge bg-dark"><?php echo $place['name'] ?></a>
-                                </div>
-                            </div>
-
-                            <div class="card-body content">
+                                    </button
                                 <a href="blog-detail-four.html" class="h5 title text-dark d-block mb-0"><?php echo $place['name'] ?></a>
                                 <p class="text-muted mt-2 mb-2"><?php echo $place['type_of_place'] ?></p> <!--city-->
                                 <a href="place-details.php?placeid=<?php echo $place['id'] ?>" class="link text-dark">Read More <i class="uil uil-arrow-right align-middle"></i></a>
                             </div>
-
                             <div class="center">
                                 <b>Reviews</b>
                                 <div class="stars"> 
