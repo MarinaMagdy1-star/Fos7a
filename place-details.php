4e576cc19931551302df93f2e9239eeffeec0fa8
Abanoubkhalil1111 <abanoubkhalil.hr23@gmail.com>
2023-05-10 22:17:08 +0300

    change

--- place-details.php
+++ place-details.php
@@ -6,6 +6,7 @@ $result =  mysqli_query($conn,$sql_query);
 $place = mysqli_fetch_assoc($result);
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include("php-includes/head-tags.php"); ?>
@@ -13,8 +14,9 @@ $place = mysqli_fetch_assoc($result);
     <body>
 
     <?php include("php-includes/navbar.php"); ?>
+   
     <!-- Body of my content -->
-    <!-- <div class="container">.
+    <!-- <div class="container">
     <div class="row justify-content-center">
                     <div class="col-lg-4 col-md-6 mt-4 pt-2">
                         <div class="card blog blog-primary shadow rounded overflow-hidden">
@@ -31,7 +33,7 @@ $place = mysqli_fetch_assoc($result);
                                 <p class="text-muted mt-2 mb-2"><?php echo $place['region'] ?></p>
                             </div>
                         </div>
-                    </div><!--end col-->
+                    </div>
 
     </div>
     </div> -->
@@ -42,25 +44,31 @@ $place = mysqli_fetch_assoc($result);
                 <div class="row">
                     <div class="col-12">
                         <div class="tiny-single-item">
+                            <?php
+                            $images_stat = "SELECT * from `place_images` where place_id='$placeId'";
+                            $images_query = mysqli_query($conn, $images_stat) or die('users_error'.mysqli_error());
+
+                            while($result = mysqli_fetch_array($images_query)){
+                            ?>
                             <div class="tiny-slide">
-                                <img src="images/portfolio/single01.jpg" class="img-fluid " alt="">
+                                <img src="Images/<?= $result['image_1_url'] ?>" class="img-fluid " alt="">
+                            </div>
+                            <div class="tiny-slide">
+                                <img src="Images/<?= $result['image_2_url'] ?>" class="img-fluid " alt="">
+                            </div>
+                            <div class="tiny-slide">
+                                <img src="Images/<?= $result['image_3_url'] ?>" class="img-fluid " alt="">
+                            </div>
+                            <div class="tiny-slide">
+                                <img src="Images/<?= $result['image_4_url'] ?>" class="img-fluid " alt="">
+                            </div>
+                            <div class="tiny-slide">
+                                <img src="Images/<?= $result['image_5_url'] ?>" class="img-fluid " alt="">
                             </div>
                             
-                            <div class="tiny-slide">
-                                <img src="images/portfolio/single02.jpg" class="img-fluid " alt="">
-                            </div>
-                            
-                            <div class="tiny-slide">
-                                <img src="images/portfolio/single03.jpg" class="img-fluid " alt="">
-                            </div>
-
-                            <div class="tiny-slide">
-                                <img src="images/portfolio/single03.jpg" class="img-fluid " alt="">
-                            </div>
-
-                            <div class="tiny-slide">
-                                <img src="images/portfolio/single03.jpg" class="img-fluid " alt="">
-                            </div>
+                            <?php
+                            }
+                            ?>
                         </div>
                     </div><!--end col-->
                 </div><!--end row-->
@@ -72,31 +80,33 @@ $place = mysqli_fetch_assoc($result);
                         <div class="section-title me-md-4">
                             <h4 class="title mb-4">Project Description</h4>
     
-                            <p class="text-muted">The most well-known dummy text is the 'Lorem Ipsum', which is said to have originated in the 16th century. Lorem Ipsum is composed in a pseudo-Latin language which more or less corresponds to 'proper' Latin. It contains a series of real Latin words. This ancient dummy text is also incomprehensible, but it imitates the rhythm of most European languages in Latin script. The advantage of its Latin origin and the relative meaninglessness of Lorum Ipsum is that the text does not attract attention to itself or distract the viewer's attention from the layout.</p>
-                            <p class="text-muted mb-0">Thus, Lorem Ipsum has only limited suitability as a visual filler for German texts. If the fill text is intended to illustrate the characteristics of different typefaces, it sometimes makes sense to select texts containing the various letters and symbols specific to the output language.</p>
+                            <p class="text-muted"> <?php echo $place ['description']  ?>  .</p>
+                            <p class="text-muted mb-0">.</p>
                         </div>
                     </div><!--end col-->
 
                     <div class="col-lg-4 col-md-6 mt-4 pt-2 mt-sm-0 pt-sm-0">
                         <div class="section-title">
-                            <h4 class="title mb-4">Project Details</h4>
+                            <h4 class="title mb-4">Place Information</h4>
                         </div>
 
                         <dl class="row mb-0">
-                            <dt class="col-md-4 fw-medium col-5">Client :</dt>
-                            <dd class="col-md-8 col-7 text-muted">Calvin Carlo</dd>
+                            <dt class="col-md-4 fw-medium col-5">prices</dt>
+                    
+                            <dd class="col-md-8 col-7 text-muted"><?php echo $place ['prices']  ?></dd>
 
-                            <dt class="col-md-4 fw-medium col-5">Category :</dt>
-                            <dd class="col-md-8 col-7 text-muted">Web Design</dd>
+                            <dt class="col-md-4 fw-medium col-5">location:</dt>
+                            <dd class="col-md-8 col-7 text-muted"><?php echo $place ['location']  ?></dd>
 
-                            <dt class="col-md-4 fw-medium col-5">Date :</dt>
-                            <dd class="col-md-8 col-7 text-muted">15th April 2021</dd>
+                            <dt class="col-md-4 fw-medium col-5">category	</dt>
+                            <dd class="col-md-8 col-7 text-muted"><?php echo $place ['region']  ?></dd>
 
-                            <dt class="col-md-4 fw-medium col-5">Website :</dt>
-                            <dd class="col-md-8 col-7 text-muted">www.yourdomain.com</dd>
+                            <dt class="col-md-4 fw-medium col-5"></dt>
+                            <dd class="col-md-8 col-7 text-muted"></dd>
 
-                            <dt class="col-md-4 fw-medium col-5">Location :</dt>
-                            <dd class="col-md-8 col-7 text-muted">3/2/64 Mongus Street, UK</dd>
+
+                            <dt class="col-md-4 fw-medium col-5"></dt>
+                            <dd class="col-md-8 col-7 text-muted"></dd>
                         </dl>
                     </div><!--end col-->
 
